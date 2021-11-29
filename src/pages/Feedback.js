7bc50ff2dd74c588d2912e887a267a88d3199c1f
Leonardo Monti <contatoleomonti@gmail.com>
2021-11-29 17:32:16 -0300

    Adiciona score e assertions na page FeedBack

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -6,23 +6,32 @@ import Header from '../components/Header';
 class Feedback extends Component {
   render() {
     const setNumber = 3;
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
+
     const feedbackMessage = (assertions < setNumber
       ? 'Podia ser melhor...' : 'Mandou bem! ');
     return (
       <>
         <Header />
-        <h1 data-testid="feedback-text">{feedbackMessage}</h1>
+        <p data-testid="feedback-text">{feedbackMessage}</p>
+        <p
+          data-testid="feedback-total-score"
+        >
+          {score}
+        </p>
+        <p data-testid="feedback-total-question">{assertions}</p>
       </>);
   }
 }
 
 Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   assertions: state.user.assertions,
+  score: state.user.score,
 });
 
 export default connect(mapStateToProps, null)(Feedback);
