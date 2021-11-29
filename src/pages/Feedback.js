62e253cb7fc922feb2f587b34f2db6b152e87e5c
Leonardo Monti <contatoleomonti@gmail.com>
2021-11-29 17:04:43 -0300

    Adiciona logica de renderização de mensagem de acertos

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,28 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
-export default class Feedback extends Component {
+class Feedback extends Component {
   render() {
+    const setNumber = 3;
+    const { assertions } = this.props;
+    const feedbackMessage = (assertions < setNumber
+      ? 'Podia ser melhor...' : 'Mandou bem! ');
     return (
       <>
         <Header />
-        <h1 data-testid="feedback-text">test</h1>
+        <h1 data-testid="feedback-text">{feedbackMessage}</h1>
       </>);
   }
 }
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  assertions: state.user.assertions,
+});
+
+export default connect(mapStateToProps, null)(Feedback);
