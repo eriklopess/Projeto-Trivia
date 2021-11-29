8bfa4c06072437b187fa9a9688eacfb7ab607c29
claudiocassimiro <days_surfing@live.com>
2021-11-29 16:00:48 -0300

    Cria componente de feedback que aparecera apos responder as 5 questoes

---
+++ src/pages/Feedback.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+export default class Feedback extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <h1>test</h1>
+      </>);
+  }
+}
